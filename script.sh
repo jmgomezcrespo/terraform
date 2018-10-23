6acf32ba48eb7b5ff7963517f4a8da15c9c6b25b
Juan Manuel Gomez Crespo <juanmanuel.gomez@eu.equinix.com>
2018-10-23 11:14:34 +0200

    script modify

--- script.sh
+++ script.sh
@@ -1,3 +1,30 @@
-#!/bin/bash
+#!/bin/bash -e
 
-echo "ejecucion script"
+BUCKET="juanma-test-plan-terraform"
+
+init() {
+ if [ -d .terraform ]; then
+   if [ -e .terraform/terraform.tfstate ]; then
+     echo "Remote state already exist!"
+     if [ -z $IGNORE_INIT ]; then
+       exit 1
+     fi
+   fi
+ fi
+
+ terraform remote config \
+   -backend=s3 \
+   -backend-config="bucket=${BUCKET}" \
+   -backend-config="key=terraform.tfstate" \
+   -backend-config="region=eu-west-1"
+
+}
+
+while getopts "i" opt; do
+ case "$opt" in
+   i)
+     IGNORE_INIT="true"
+     ;;
+ esac
+done
+
